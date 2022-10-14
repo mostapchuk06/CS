d98d5fc820d9855c071ea34e923888c936313400
mostapchuk06 <101934840+mostapchuk06@users.noreply.github.com>
2022-10-14 16:14:46 +0300

    Add files via upload

---
+++ cs_strings/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace strings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Task 1
+            Console.WriteLine("\t...Task.1...");
+            Console.WriteLine(String.Join(" ", "Some sentence 0_0".Split().Reverse()) + "\n\n");
+
+            //Task 2
+            Console.WriteLine("\t...Task.2...");
+            string str;
+            int upper = 0;
+            Console.WriteLine("Enter sentence : ");
+            str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                if (ch >= 'A' && ch <= 'Z')
+                    upper++;
+            }
+            Console.Write("Characters in uppercase: {0}\n\n", upper);
+
+            //Task 3
+            Console.WriteLine("\t...Task.3...");
+            Console.WriteLine("Enter sentence : ");
+            string str2 = Console.ReadLine();
+            string[] words = str2.Split(' ', '.', ',', '!', '?');
+            int most_long = 0;
+            foreach (string word in words)
+            {
+                if (word.Length > most_long)
+                {
+                    most_long = word.Length;
+                }
+            }
+            Console.WriteLine(most_long);
+
+            //Task 4
+            Console.WriteLine("\t...Task.4...");
+            Console.WriteLine("Enter sentence : ");
+            string str3 = Console.ReadLine(); 
+            Console.WriteLine(str3.Replace('.', '!'));
+        
+        }
+    }
+}
