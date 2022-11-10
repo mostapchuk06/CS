1db8a66c8bd069c4c9f01da1f7ac7772b7f8a25d
mostapchuk06 <101934840+mostapchuk06@users.noreply.github.com>
2022-11-10 21:26:33 +0200

    Add files via upload

---
+++ Musical_Instruments/Musical_Instruments/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Task
+{
+    internal class Program
+    {
+        public class Instrument
+        {
+            public string name { get; set; }
+            public string type { get; set; }
+            public string color { get; set; }
+            
+            public virtual void Show()
+            {
+                Console.WriteLine();
+            }
+        }
+        public class Guitar : Instrument
+        {
+            public string material { get; set; }
+            public Guitar(string name, string type, string color, string material)
+            {
+                this.name = name;
+                this.type = type;
+                this.color = color;
+                this.material = material;
+            }
+            public override void Show()
+            {
+                Console.WriteLine($".................{type}....................");
+                Console.WriteLine($"\t   {name}");
+                Console.WriteLine($"\t   {color}");
+                Console.WriteLine($"\t   {material}");
+                Console.WriteLine($"...........................................\n");
+            }
+        }
+        public class BassGuitar : Instrument
+        {
+            public string material { get; set; }
+            public BassGuitar(string name, string type, string color, string material)
+            {
+                this.name = name;
+                this.type = type;
+                this.color = color;
+                this.material = material;
+            }
+            public override void Show()
+            {
+                Console.WriteLine($"..............{type}..................");
+                Console.WriteLine($"\t   {name}");
+                Console.WriteLine($"\t   {color}");
+                Console.WriteLine($"\t   {material}");
+                Console.WriteLine($"...........................................\n");
+            }
+        }
+        public class ElectroGuitar : Instrument
+        {
+            public string material { get; set; }
+            public ElectroGuitar(string name, string type, string color, string material)
+            {
+                this.name = name;
+                this.type = type;
+                this.color = color;
+                this.material = material;
+            }
+            public override void Show()
+            {
+                Console.WriteLine($"..............{type}...............");
+                Console.WriteLine($"\t   {name}");
+                Console.WriteLine($"\t   {color}");
+                Console.WriteLine($"\t   {material}");
+                Console.WriteLine($"...........................................\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Guitar g = new Guitar("Gipson", "Guitar", "Red", "Wood");
+            g.Show();
+            BassGuitar bg = new BassGuitar("Rickenbacker 4001 V63", "Bass Guitar", "Black", "Wood");
+            bg.Show();
+            ElectroGuitar eg = new ElectroGuitar("Competition Mustang 1969", "Electro Guitar", "Lake Placid Blue", "Wood");
+            eg.Show();
+        }
+    }
+}
